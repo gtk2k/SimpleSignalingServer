0be2f41e96baf8db2344ce556a0e389bcea80601
gtk2k <gtk2k@yahoo.co.jp>
2022-04-18 14:07:33 +0900

    Add files via upload

---
+++ SignalingServer.js
@@ -0,0 +1,36 @@
+import { connect } from 'http2';
+import {WebSocketServer} from 'ws';
+import {v4 as uuidv4} from 'uuid';
+const peers = {};
+const wss = new WebSocketServer({port:8989});
+let sender, receiver;
+wss.on('connection', (ws) => {
+    console.log('on connection');
+    ws.on('message', (data) => {
+        console.log(data.toString());
+        const msg = JSON.parse(data);
+        console.log(msg.src, msg.peerType, msg.type);
+        if(msg.type === 'new'){
+            ws.peerType = msg.peerType;
+            if(msg.peerType === 'sender'){
+                sender = ws;
+            }
+            else
+            {
+                receiver = ws;
+            }
+        }
+        if(ws.peerType === 'receiver'){
+            sender.send(JSON.stringify(msg));
+        }
+        else
+        {
+            receiver?.send(JSON.stringify(msg));
+        }
+    });
+    ws.on('close', ws => {
+        if(peers[ws.peerId]){
+            delete peers[ws.peerId];
+        }
+    });
+});
